3802503fe8a64779b1701233593d4cba8f158b3a
Logan Elandt <elandtdevelops@gmail.com>
2020-05-08 17:43:10 -0500

    Add tests during Django Tutorial Part 5

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -18,8 +19,16 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        """Return the last five pulished questions."""
-        return Question.objects.order_by("-pub_date")[:5]
+        """
+        Return the last five pulished questions (not
+        including those set to be published in the
+        future).
+        """
+        now = timezone.now()
+        order = "-pub_date"
+        # __lte filters for pub_date less than or equal
+        # to the provided value
+        return Question.objects.filter(pub_date__lte=now).order_by(order)[:5]
 
 
 class DetailView(generic.DetailView):
@@ -27,6 +36,12 @@ class DetailView(generic.DetailView):
     # Overrides the default template name of DetailView
     # generic, default is <app name>/<model name>_detail.html
     template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
