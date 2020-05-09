bbc06f382342005c67c85276ce3d708e947b8f80
Logan Elandt <elandtdevelops@gmail.com>
2020-05-09 11:27:14 -0500

    Change Choices to be edited from the Question they belong to using admin.StckedInline

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -5,15 +5,26 @@ from .models import Choice, Question
 # Register your models here.
 
 
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    # Provide enough fields for 3
+    # choices when editing choices on
+    # the Question admin page in addition to
+    # the fields for choices that exist
+    # for the Question already
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {"fields": ["question_text"]}),
         # The first value is the name of the set
         # it will display on the admin page
         # as a section header
-        ("Date Information", {"fields": ["pub_date"]}),
+        ("Date Information", {"fields": ["pub_date"], "classes": ["collapse"]})
     ]
+    # Adds the ChoiceInLine class to the QuestionAdmin page
+    inlines = [ChoiceInLine]
 
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
