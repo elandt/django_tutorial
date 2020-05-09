8d20e78d7e6a5ae947257b776764fe7327d3b390
Logan Elandt <elandtdevelops@gmail.com>
2020-05-09 11:42:29 -0500

    Change how the result of was_published_recently() displays on the admin page for Question selection

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -23,6 +23,12 @@ class Question(models.Model):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
+    # These attributes need to come after the method definition
+    # They provide Django with info about how
+    # to display the result of the method on the admin page
+    was_published_recently.admin_order_field = "pub_date"
+    was_published_recently.boolean = True
+    was_published_recently.short_description = "Published recently?"
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
